b8cbb06ae799ffef2104297955ac5b0732a7c054
emilyngodby <egodby@nevada.unr.edu>
2021-03-15 23:32:00 -0700

    fixed auto-scrolling in chatbot.html

--- django/VRRT/train.py
+++ django/VRRT/train.py
@@ -1,4 +1,5 @@
 from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 chatbot = ChatBot('Chatterbot',
@@ -7,11 +8,13 @@ storage_adapter='chatterbot.storage.SQLStorageAdapter',
 database_uri='sqlite:///database.db')
 
 trainer = ChatterBotCorpusTrainer(chatbot)
-trainer.train(
-    "chatterbot.corpus.english.greetings",
-    #"chatterbot.corpus.bangla.botprofile",
-    #"chatterbot.corpus.buses.route3",
-    #"chatterbot.corpus.buses.broute",
-    "chatterbot.corpus.english.conversations",
 
-)
+trainer.train("TrainingDocs/intents.yml")
+# trainer.train(
+#     "chatterbot.corpus.english.greetings",
+#     #"chatterbot.corpus.bangla.botprofile",
+#     #"chatterbot.corpus.buses.route3",
+#     #"chatterbot.corpus.buses.broute",
+#     "chatterbot.corpus.english.conversations",
+
+# )
