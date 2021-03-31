ed5035f8819a127154789d2a97262f74d259cc95
stoldalc <christianstoldal@gmail.com>
2021-03-31 11:55:24 -0700

    Added custom forgot password page

--- VRRT/settings.py
+++ VRRT/settings.py
@@ -158,3 +158,7 @@ LOGIN_REDIRECT_URL = 'logInRedirect'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
